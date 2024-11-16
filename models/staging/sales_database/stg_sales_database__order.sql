d77a99f5781d483ca9b2a254bff5686e63508e41
BarbeFeu <jbp.computing@gmail.com>
2024-11-17 00:25:35 +0100

    Lololo

--- models/staging/sales_database/stg_sales_database__order.sql
+++ models/staging/sales_database/stg_sales_database__order.sql
@@ -2,9 +2,9 @@ select
     order_id AS order_id_y,
     user_name as user_id_y,
     order_status AS order_status_y,
-    DATETIME(order_date, "Europe/Paris") AS order_created_at_y,
-    DATETIME(order_approved_date, "Europe/Paris") AS order_approved_at_y,
-    DATETIME(pickup_date, "Europe/Paris") AS picked_up_at_y,
-    DATETIME(delivered_date, "Europe/Paris") AS delivered_at_y,
-    DATETIME(estimated_time_delivery, "Europe/Paris") AS estimated_time_delivery_y
+    DATETIME(order_date, "Europe/Paris") AS order_created_at_z,
+    DATETIME(order_approved_date, "Europe/Paris") AS order_approved_at_z,
+    DATETIME(pickup_date, "Europe/Paris") AS picked_up_at_z,
+    DATETIME(delivered_date, "Europe/Paris") AS delivered_at_z,
+    DATETIME(estimated_time_delivery, "Europe/Paris") AS estimated_time_delivery_z
 from {{ source('sales_database', 'order') }}
